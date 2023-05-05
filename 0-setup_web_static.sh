f81aebed5c5e6597b1b41a39d506ab57a2b4ac5c
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-05-05 21:24:37 +0000

    File That Prepare My Web-Server For Static Web Deployment

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# This program will install Nginx and Put a static file in it.
+# This bash script will automate the creation of files.
+
+#sudo apt update
+#sudo apt-get install nginx -y
+#sudo ufw allow "Nginx HTTP"
+
+# Creating All Required Folders and File
+sudo mkdir -p /data/web_static/releases
+sudo mkdir -p /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/current
+sudo touch /data/web_static/releases/test/index.html
+
+PATH_F=/data/web_static/releases/test/index.html
+
+# Adding a Test Content to index file
+CONTENT="<html>
+<head>
+</head>
+<body>
+    Holberton School
+</body>
+</html>"
+
+echo "$CONTENT" | sudo tee "$PATH_F"
+
+# Creating A Symbolic Linking
+#cd /data/web_static/current
+sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
+
+# Granting Ownership
+sudo chown -R ubuntu:ubuntu /data/
+
+# Creating hbtn dir
+#mkdir hbtn_static
+
+# Here is the section that configures Nginx Config file
+CONF_PATH=/etc/nginx/sites-enabled/default
+
+sudo sed -i "/listen 80 default_server;/a\\\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}" "$CONF_PATH"
+
+sudo service nginx restart
