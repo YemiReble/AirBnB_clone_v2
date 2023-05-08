ce62caf780eb01596c5ba60311c42b38179c9e85
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-05-08 14:41:41 +0000

    Creating And Compressing My Web_Static Files for Deployment

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,8 +5,8 @@ file to my server, by first compressing it.
 """
 
 from fabric.api import local
-# from time import strftime
-# from datetime import time
+from time import strftime
+from datetime import time
 
 
 def do_pack():
@@ -14,10 +14,11 @@ def do_pack():
     do_pack funtion or method will create a folder called
     version and create web_static achive in it with date
     """
+    filedate = strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p version") 
-        local("tar -czvf version/web_static_$(%Y%m%d%H%M%s).tgz web_static")
-        return "version/web_static_$(%Y%m%d%H%M%s).tgz"
+        local("tar -czvf version/web_static_{}.tgz web_static".format(filedate))
+        return "version/web_static_{}.tgz".format(filedate)
 
     except Exception as e:
         return None
