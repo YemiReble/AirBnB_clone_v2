920ce97d79c1656235de3d31f248712182ae7f49
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-05-08 14:55:05 +0000

    Creating And Compressing My Web_Static Files for Deployment

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -16,7 +16,7 @@ def do_pack():
     """
     filedate = strftime("%Y%m%d%H%M%S")
     try:
-        local("mkdir -p version")
+        local("mkdir -p versions")
         local("tar -czvf versions/web_static_{}.tgz web_static".format(
             filedate))
         return "versions/web_static_{}.tgz".format(filedate)
