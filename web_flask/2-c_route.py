af5bc48796e9724138d4efaf33a4faff4b638c93
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-05-21 16:52:42 +0000

    Requsting Data from a folder that concatinate strings with a default string C

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This is my first Flask program using python
+"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+@app.route('/')
+def hello():
+    """Function that return Hello HBNB when user
+        make a request to our ip address.
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """The function display "HBNB"
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c(text):
+    """This c function returns multiple
+    stuff on the screen"""
+    return "C " + text.replace("_", " ")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
