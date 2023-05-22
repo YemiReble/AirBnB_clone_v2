a882bd834ce27ab9b350beeb65eb6f5e45c7256b
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-05-22 14:48:05 +0000

    Number Route

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -33,23 +33,20 @@ def c(text):
 
 
 @app.route('/python')
-@app.route('python/<text>')
+@app.route('/python/<text>')
 def python(text='is cool'):
     """This fucntion returns a default text "Python is"
     with an argument passed to it
     """
-    return 'Python {}'.format(text.replace('_', ' '))
+    return "Python {}".format(text.replace("_", " "))
 
 
-@app.route('/number/<int:n>')
-def number(n):
-    """This function return integer and interger only
-    :n param: being the varable that holds the integer
+@app.route('/number/<n>')
+def number(int: n):
+    """This route fuction allow only integers to be
+    passed into it, if not integer, it fails
     """
-    return '{} is a number'.format(n)
-
-    # if __name__ == '__main__':
-    #    app.run(host='0.0.0.0', port=5000, debug=True, strict_slashes=False)
+    return "{} is a number".format(n)
 
 
 if __name__ == "__main__":
