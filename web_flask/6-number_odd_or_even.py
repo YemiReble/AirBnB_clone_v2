809645681b45a61838aed371d029175ec7f538b8
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-05-22 17:35:21 +0000

    Odd or Even

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+"""
+This is my first Flask program using python
+"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """Function that return Hello HBNB when user
+        make a request to our ip address.
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """The function display "HBNB"
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c(text):
+    """This c function returns multiple
+    stuff on the screen"""
+    return "C " + text.replace("_", " ")
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python(text='is cool'):
+    """This fucntion returns a default text "Python is"
+    with an argument passed to it
+    """
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    """
+    Displays "<n> is a number" when the /number/<n> URL is accessed,
+            but only if n is an integer.
+    Args:
+        n (int): The number to display.
+    Returns:
+        str: The complete string to display, or an error message
+        if n is not an integer.
+    """
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    """
+    Display An HTML page when the /number_template/<n> URL is accessed.
+    Args:
+        n (int): The number that when call it displays and HTML page
+    Returns:
+        str: The complete HTML Page to display.
+    """
+    return render_template('templates/5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<n>')
+def odd_or_even(n):
+    """
+    Display an HTML page when
+    Args:
+        n (int): The number to call
+    Returns:
+        str: The HTML Pageto display.
+    """
+    return render_template(
+        '6-number_odd_or_even.html', n=n, parity='even' if n % 2 == 0 else 'odd')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
