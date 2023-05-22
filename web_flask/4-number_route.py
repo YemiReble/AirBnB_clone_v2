515cb644051d208dc24b9b821e73c36c217a39ef
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-05-22 14:31:11 +0000

    Number Route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+This is my first Flask program using python
+"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """Function that return Hello HBNB when user
+        make a request to our ip address.
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """The function display "HBNB"
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c(text):
+    """This c function returns multiple
+    stuff on the screen"""
+    return "C " + text.replace("_", " ")
+
+
+@app.route('/python')
+@app.route('python/<text>')
+def python(text='is cool'):
+    """This fucntion returns a default text "Python is"
+    with an argument passed to it
+    """
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    return '{} is a number'.format(n)
+
+# if __name__ == '__main__':
+#    app.run(host='0.0.0.0', port=5000, debug=True, strict_slashes=False)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
