b87238382283efc2ff550a596de0aecffda35da0
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-05-23 17:50:50 +0000

    db Storage Update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -29,8 +29,8 @@ class DBStorage:
     def __init__(self):
         """initialize the db storage"""
         self.__engine = create_engine(
-                'mysql+mysqldb://{}:{}@{}:3306/{}'.format(usr, pwd, host, db),
-                pool_pre_ping=True)
+            'mysql+mysqldb://{}:{}@{}:3306/{}'.format(usr, pwd, host, db),
+            pool_pre_ping=True)
         if env == 'test':
             Base.metadata.drop_all()
 
@@ -39,15 +39,15 @@ class DBStorage:
         if cls is not None:
             objs = self.__session.query(cls).all()
             objs_dict = {
-                    f'{obj.__class__.__name__}.{obj.id}': obj
-                    for obj in objs}
+                f'{obj.__class__.__name__}.{obj.id}': obj
+                for obj in objs}
         else:
             objs_dict = {}
             for cls in [User, State, City, Review, Place, Amenity]:
                 objs = self.__session.query(cls).all()
                 objs_dict.update({
-                        f'{obj.__class__.__name__}.{obj.id}': obj
-                        for obj in objs})
+                    f'{obj.__class__.__name__}.{obj.id}': obj
+                    for obj in objs})
         return objs_dict
 
     def new(self, obj):
@@ -67,7 +67,12 @@ class DBStorage:
         """Reloads all objects from database"""
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(
-                bind=self.__engine,
-                expire_on_commit=False)
+            bind=self.__engine,
+            expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+
+    def def close(self):
+        """Close session if active"""
+        self.__session.remove()
