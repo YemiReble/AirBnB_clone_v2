1c2e3fa99139ff582583c8f6213aa941371a6bc0
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-05-24 11:51:10 +0000

    Updated version

--- models/state.py
+++ models/state.py
@@ -19,27 +19,26 @@ class State(BaseModel, Base):
             backref='state')
     else:
         name = ''
-		"""
+        """
         @property
         def cities(self):
             from models import storage
             from models.city import City
             return [city for city in storage.all(City)
                     if city.state_id == self.id]
-		"""
+        """
 
+        def __init__(self, *args, **kwargs):
+            """initializing State"""
+            super().__init__(*args, **kwargs)
 
-	def __init__(self, *args, **kwargs):
-    	"""initializes state"""
-    	super().__init__(*args, **kwargs)
-
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
-        @property
-        def cities(self):
-            """fs getter attribute that returns City instances"""
-            values_city = models.storage.all("City").values()
-            list_city = []
-            for city in values_city:
-                if city.state_id == self.id:
-                    list_city.append(city)
-            return list_city
+        if getenv('HBNB_TYPE_STORAGE') != 'db':
+            @property
+            def cities(self):
+                """fs getter attribute that returns City instances"""
+                values_city = models.storage.all("City").values()
+                list_city = []
+                for city in values_city:
+                    if city.state_id == self.id:
+                        list_city.append(city)
+                return list_city
