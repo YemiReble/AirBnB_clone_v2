b66e043674377eccc213b2862d4fb04d2d8bd302
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-05-24 11:29:04 +0000

    Update On Cities

--- models/city.py
+++ models/city.py
@@ -14,10 +14,14 @@ class City(BaseModel, Base):
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
         places = relationship(
-                'Place',
-                cascade='all, delete-orphan',
-                backref='cities')
+            'Place',
+            cascade='all, delete-orphan',
+            backref='cities')
     else:
         state_id = ''
         name = ''
         places = ''
+
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
